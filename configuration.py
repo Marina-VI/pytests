500d173b5b71a43f8c4fee2c65f47949640d0e19
Marina-VI <Marina.ignatova1998>
2023-06-12 14:20:05 +0300

    initial

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://de629895-fe87-4e7a-977b-ff1ee841cccf.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
